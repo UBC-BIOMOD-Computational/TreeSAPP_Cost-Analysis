7e83fc96fcf8b24a1dc2b55e07539751d1f305f0
cherr <charity.grey1@gmail.com>
2025-09-02 15:10:21 -0700

    scaffold model

---
+++ Cost Analysis/models/cost_model.py
@@ -0,0 +1,33 @@
+import conversion as cv
+import pandas as pd
+import numpy as np
+
+## =================================================================
+## CELL FREE =======================================================
+
+#  Cell Free Lab ---------------------------------------------------
+
+#  Cell Free Industry ----------------------------------------------
+
+#  Cell Free Prototype ---------------------------------------------
+
+#  Cell Free Lab (recycle mpads) -----------------------------------
+
+#  Cell Free Industry (recycle mpads) ------------------------------
+
+#  Cell Free Prototype (recycle mpads) -----------------------------
+
+#  Cell Free Prototype (Switching Proteins) ------------------------
+
+#  Cell Free Prototype (recycle mpads) (Switching Proteins) --------
+
+## =================================================================
+## CELL BASED ======================================================
+
+#  Cell Based Lab --------------------------------------------------
+
+#  Cell Based Industry ---------------------------------------------
+
+#  Cell Based Prototype --------------------------------------------
+
+#  Cell Based Prototype (Switching Proteins) -----------------------
