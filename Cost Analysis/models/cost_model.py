193afa0a36f63eb6355a121d56304af87d43c133
cherr <charity.grey1@gmail.com>
2025-09-02 15:44:00 -0700

    tried prototype cfpp but majority of costs still lysate

--- Cost Analysis/models/cost_model.py
+++ Cost Analysis/models/cost_model.py
@@ -8,9 +8,9 @@ final_costs_dict = []
 ## CELL FREE =======================================================
 
 #  Cell Free Lab ---------------------------------------------------
-cfpp_df = pd.read_csv("data/CFPP_lab_20kg_100cycles.csv")
+cfpp_lab_df = pd.read_csv("data/CFPP_lab_20kg_100cycles.csv")
 def cfpp_lab(name):
-    return cfpp_df.loc[cfpp_df['name'] == name, 'value'].iloc[0]
+    return cfpp_lab_df.loc[cfpp_lab_df['name'] == name, 'value'].iloc[0]
 
 '''
 cfpp_initial_cost = mold equipment + photoresist + pdms for mold creation
@@ -48,12 +48,56 @@ final_costs_dict.append({
     "initial_cost": cfpp_initial_cost,
     "cycle_cost_$": cfpp_per_cycle_cost,
     "cycle_time_hours": cfpp_time,
-    "protein_per_cycle_mg": 200 / 3
+    "protein_per_cycle_mg": cfpp_lab('Target Protein Per Cycle')
 })
 
 #  Cell Free Industry ----------------------------------------------
+# TODO 
+
 
 #  Cell Free Prototype ---------------------------------------------
+cfpp_proto_df = pd.read_csv("data/CFPP_prototype_10g_10cycles.csv")
+def cfpp_proto(name):
+    return cfpp_proto_df.loc[cfpp_proto_df['name'] == name, 'value'].iloc[0]
+
+'''
+cfpp_initial_cost = mold equipment + photoresist + pdms for mold creation
+'''
+cfpp_initial_cost = 77000 + 750 + cfpp_proto('Total PDMS Volume') * 340
+
+'''
+"$/per cycle = 
+=  lysate at 602CAD/0.5L
++ dna sequences for xdna arms at $100/mg
++ T4 Ligase for 243units/$1
++ T4 Ligase buffer for $10/mL
++ ApaI for 50 units/$1
++ plasmid purified (7500ug for $300)"
+'''
+cost_lysate = cfpp_proto('Total Lysate') * 2 * 602
+cost_xdna = cv.convert_units_grams(cfpp_proto('Total xDNA'), 'ug', 'mg') * 100
+cost_t4_ligase = cfpp_proto('Total T4 Ligase') / 243
+cost_t4_buffer = cfpp_proto('Total T4 Ligase Buffer') * 10
+cost_apaI = cfpp_proto('Total ApaI Volume') / 50
+cost_plasmid = cv.convert_units_grams(cfpp_proto('Total Gene Plasmid'), 'ng', 'ug') / 7500 * 300
+print('cost_lysate + cost_xdna + cost_t4_ligase + cost_t4_buffer + cost_apaI + cost_plasmid')
+print(cost_lysate, cost_xdna, cost_t4_ligase, cost_t4_buffer, cost_apaI, cost_plasmid)
+cfpp_per_cycle_cost = cost_lysate + cost_xdna + cost_t4_ligase + cost_t4_buffer + cost_apaI + cost_plasmid
+
+'''
+"t = 40h (pipelined into upstream + downstream) 
+*not including initial mold making"
+'''
+cfpp_time = 40
+
+final_costs_dict.append({
+    "name": "CFPP Prototype",
+    "num_cycles": cfpp_proto('Total Cycles'),
+    "initial_cost": cfpp_initial_cost,
+    "cycle_cost_$": cfpp_per_cycle_cost,
+    "cycle_time_hours": cfpp_time,
+    "protein_per_cycle_mg": cfpp_proto('Target Protein Per Cycle')
+})
 
 #  Cell Free Lab (recycle mpads) -----------------------------------
 
@@ -73,7 +117,9 @@ final_costs_dict.append({
 "$  =  60000
 = 10000 +  30000 + 20000
 = stable cell line development + cellular expression equipment + highPressureHomogenizer/centrifuge "
-'''
+'''# TODO 
+
+
 cbpp_initial_cost = 0
 
 '''
